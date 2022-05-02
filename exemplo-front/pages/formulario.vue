624610e1298cb42ed5d518bb1d4f72d4b1fc8d1f
giovannalauraa <giovanna.laura@aluno.ifsp.edu.br>
2022-05-02 16:43:45 -0300

    Criação exemplo Nuxt.js

---
+++ exemplo-front/pages/formulario.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+      <h3>Insira seus dados</h3>
+      <form @submit.prevent="digaOla">
+          <span ref="nome">Nome: </span>
+          <input id="nome" v-model="form.nome" type="text" /> <br><br>
+          <span ref="email"> Email: </span>
+          <input id="email" v-model="form.email" type="email"/> <br><br>
+          <button type="submit">Ok</button>
+      </form>
+
+      <div v-show="form.nome">
+          <h4 v-if = "form.nome && form.email">
+              Seus dados já podem ser enviados!
+            </h4>
+              <span v-else style="color: red;">Dados incompletos!</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            form: {
+                nome: '',
+                email: '',
+            },
+        }
+    },
+    methods: {
+        digaOla: function (){
+            alert(JSON.stringfy(this.form))
+        },
+    },
+
+}
+</script>
+
+<style scoped>
+.container{
+    margin: 20px;
+}
+
+</style>
---
+++ exemplo-front/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+   <h1>Hello world!</h1>
+   <ul>
+     <li>Elemento 1</li>
+     <li>Elemento 2</li>
+     <li>Elemento 3</li>
+     <li>Elemento 4</li>
+
+   </ul>
+ </div>
+</template>
+
+<script>
+
+export default {
+  name: 'IndexPage'
+}
+</script>
